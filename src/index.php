58bd905db6c570d4b2f130b739f1f9fa91df772f
Kevin Gregory <medkg15@gmail.com>
2014-10-23 20:18:58 -0400

    Build out application framework.

--- src/index.php
+++ src/index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <html>
 <head>
     <link href="/css/main.css" rel="stylesheet" type="text/css"/>
@@ -6,42 +6,142 @@
 <body>
 <div class="container">
 
-	<nav class="navbar navbar-default" role="navigation">
-  <div class="container-fluid">
-  
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#" data-bind="click: returnHome">How To Train Your Robot</a>
+    <nav class="navbar navbar-default" role="navigation">
+        <div class="container-fluid">
+
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#" data-bind="click: returnHome">Digital Farmington</a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <div class="row">
+        <div class="col-md-9">
+
+            <div id="map">
+
+            </div>
+
+            <div>
+
+                <h2>Select a Year</h2>
+
+                <button class="btn btn-default">1640</button>
+                <button class="btn btn-default">1700</button>
+                <button class="btn btn-default">1730</button>
+                <button class="btn btn-default">1780</button>
+                <button class="btn btn-default">1800</button>
+                <button class="btn btn-default">1850</button>
+                <button class="btn btn-default">1880</button>
+                <button class="btn btn-default">1910</button>
+                <button class="btn btn-default">1950</button>
+                <button class="btn btn-default">1980</button>
+                <button class="btn btn-default">2020</button>
+
+            </div>
+
+        </div>
+        <div class="col-md-3">
+
+            <h3>Filter Categories</h3>
+
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox"/> Stuff
+                </label>
+            </div>
+
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox"/> Stuff
+                </label>
+            </div>
+
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox"/> Stuff
+                </label>
+            </div>
+
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox"/> Stuff
+                </label>
+            </div>
+
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox"/> Stuff
+                </label>
+            </div>
+
+
+        </div>
     </div>
 
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-          <li><a href="#" data-bind="click: function(){ changeView('about'); }">About</a></li>
-          <li><a href="#" data-bind="click: function(){ changeView('high-scores'); }">High Scores</a></li>
-      </ul>
-    
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Score: <span data-bind="text: score"/></a></li>
-       
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
+    <h2>Historic Maps</h2>
 
-    </div><!-- /.container -->
+    <div class="row">
+        <div class="col-md-2">
+            <a class="thumbnail" data-toggle="modal" data-target="#myModal">
+                <img src="http://placehold.it/350x150" alt="...">
+            </a>
+        </div>
+        <div class="col-md-2">
+            <a class="thumbnail" data-toggle="modal" data-target="#myModal">
+                <img src="http://placehold.it/350x150" alt="...">
+            </a>
+        </div>
+        <div class="col-md-2">
+            <a class="thumbnail" data-toggle="modal" data-target="#myModal">
+                <img src="http://placehold.it/350x150" alt="...">
+            </a>
+        </div>
+        <div class="col-md-2">
+            <a class="thumbnail" data-toggle="modal" data-target="#myModal">
+                <img src="http://placehold.it/350x150" alt="...">
+            </a>
+        </div>
+        <div class="col-md-2">
+            <a class="thumbnail" data-toggle="modal" data-target="#myModal">
+                <img src="http://placehold.it/350x150" alt="...">
+            </a>
+        </div>
+        <div class="col-md-2">
+            <a class="thumbnail" data-toggle="modal" data-target="#myModal">
+                <img src="http://placehold.it/350x150" alt="...">
+            </a>
+        </div>
+    </div>
+
+    <?php include "modal.php"; ?>
+
+</div>
+<!-- /.container -->
 <script src="/vendor/requirejs/require.js"></script>
 <script>
-	requirejs.config({
-		baseUrl:  "<?php echo (strpos($_SERVER["HTTP_HOST"], 'amazonaws.com') !== false) ? '/js-built' : '/js'; ?>",
-	});
-	require(['common'], function(common){
-		/*require(
-			['jquery', 'knockout', 'app/gameViewModel', 'jstree'],
-			function($, ko, GameViewModel, jstree){
-				
-				
-			}
-		);*/
-	});
+    requirejs.config({
+        baseUrl: "<?php echo (strpos($_SERVER["HTTP_HOST"], 'amazonaws.com') !== false) ? '/js-built' : '/js'; ?>",
+    });
+    require(['common'], function (common) {
+        require(
+            ['bootstrap', 'jquery', 'async!http://maps.google.com/maps/api/js?sensor=false&key=AIzaSyD0K-y2C9IH2lUf6_kOt8Dvd9TOlZq7sqk'],
+            function(bootstrap, $, googleMaps){
+
+                var mapOptions = {
+                    center: { lat: -34.397, lng: 150.644},
+                    zoom: 8
+                };
+                var map = new google.maps.Map(document.getElementById('map'),
+                    mapOptions);
+        });
+    });
 </script>
 </body>
 </html>
--- src/js/common.js
+++ src/js/common.js
@@ -3,6 +3,7 @@ requirejs.config({
 		"jquery": "../vendor/jquery/dist/jquery.min"
         , "bootstrap": "../vendor/bootstrap/dist/js/bootstrap.min"
         , "underscore": "../vendor/underscore/underscore-min"
+        , 'async': '/vendor/requirejs-plugins/src/async'
 	}
 	, shim: {
 		"bootstrap": ['jquery']
