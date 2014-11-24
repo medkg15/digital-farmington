b75d686976f9c4ca5e965ddc62c71ee4b63c79bc
nella14 <marianellaryd@gmail.com>
2014-11-24 13:39:08 -0500

    map image
    map image

--- src/index.php
+++ src/index.php
@@ -73,7 +73,7 @@ $categories = $data_access->get_categories();
 		 <p>1800 CT Map  <p/>
 		 <img src="images/linePic2.png" alt="line" width="180" />
             <a class="thumbnail" data-toggle="modal" data-target="#myModal">
-                <img src="images/1800sCT.jpg" alt="Historical Maps" width="350" height="150"/>
+                <img src="images/1800sCT2.jpg" alt="Historical Maps" width="350" height="150"/>
             </a>
         </div>
         <div class="col-md-2">
