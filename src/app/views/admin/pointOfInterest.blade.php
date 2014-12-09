865fc9b7251e34d2a45e43a639a717eb095ef3cb
Kevin Gregory <medkg15@gmail.com>
2014-12-09 00:35:14 -0500

    implement category and year setting for pois.

--- src/app/views/admin/pointOfInterest.blade.php
+++ src/app/views/admin/pointOfInterest.blade.php
@@ -11,50 +11,65 @@
 
 
     <div class="row">
-    <div class="col-md-8">
+        <div class="col-md-8">
 
-            <div class="form-group">
-                {{Form::label('name', 'Name')}}
-                {{ Form::text('name', Input::old('name'), array('class' => 'form-control')) }}
+                <div class="form-group">
+                    {{Form::label('name', 'Name')}}
+                    {{ Form::text('name', Input::old('name'), array('class' => 'form-control')) }}
 
-            </div>
+                </div>
 
-            <div class="form-group">
-                {{Form::label('description', 'Description')}}
-                {{ Form::textarea('description', Input::old('description'), array('class' => 'form-control')) }}
-            </div>
+                <div class="form-group">
+                    {{Form::label('description', 'Description')}}
+                    {{ Form::textarea('description', Input::old('description'), array('class' => 'form-control')) }}
+                </div>
 
-            <div class="form-group">
-                {{Form::label('latitude', 'Latitude')}}
-                {{ Form::text('latitude', Input::old('latitude') ? Input::old('latitude') : Input::get('latitude'), array('class' => 'form-control')) }}
+                <div class="form-group">
+                    {{Form::label('latitude', 'Latitude')}}
+                    {{ Form::text('latitude', Input::old('latitude') ? Input::old('latitude') : Input::get('latitude'), array('class' => 'form-control')) }}
 
-            </div>
+                </div>
 
-            <div class="form-group">
-                {{Form::label('longitude', 'Longitude')}}
-                {{ Form::text('longitude', Input::old('longitude') ? Input::old('longitude') :  Input::get('longitude'), array('class' => 'form-control')) }}
-            </div>
+                <div class="form-group">
+                    {{Form::label('longitude', 'Longitude')}}
+                    {{ Form::text('longitude', Input::old('longitude') ? Input::old('longitude') :  Input::get('longitude'), array('class' => 'form-control')) }}
+                </div>
 
-            <div class="checkbox">
-              <label>
-                {{Form::checkbox('display', 'true', true)}}
-                Show On Map?
-              </label>
-            </div>
-    </div>
-    <div class="col-md-4">
+                <div class="checkbox">
+                  <label>
+                    {{Form::checkbox('display', '1', true)}}
+                    Show On Map?
+                  </label>
+                </div>
+        </div>
+        <div class="col-md-4">
 
-        <h3>Map Years</h3>
+            <h3>Map Years</h3>
 
-        @foreach($eras as $era)
-            <div class="checkbox">
-              <label>
+            @foreach($eras as $era)
+                <div class="checkbox">
+                  <label>
 
-                {{print_r(in_array($era->id, array()) == true);}}
+                    {{Form::checkbox('era[]', $era->id, $poi && $poi->eras && in_array($era->id, array_map(function($era){ return $era['id']; }, $poi->eras->toArray())))}}
+                    {{$era->label}}
 
-              </label>
-            </div>
-        @endforeach
+                  </label>
+                </div>
+            @endforeach
+
+            <h3>Categories</h3>
+
+            @foreach($categories as $category)
+                <div class="checkbox">
+                  <label>
+
+                    {{Form::checkbox('category[]', $category->id, $poi && $poi->categories && in_array($category->id, array_map(function($category){ return $category['id']; }, $poi->categories->toArray())))}}
+                    {{$category->label}}
+
+                  </label>
+                </div>
+            @endforeach
+            <div id="add-wrapper"><input type="text" name="new_category"/><button id="add">Add</button></div>
 
         </div>
     </div>
@@ -66,4 +81,20 @@
 @stop
 
 @section('scripts')
+<script type="text/javascript">
+
+require(['common'], function(){
+    require(['jquery'], function($){
+        $('#add').click(function(e){
+            e.preventDefault();
+            var newCategory = $('[name=new_category]').val();
+
+            if(newCategory && $('input[value=' + newCategory + ']').length === 0)
+            {
+                $('#add-wrapper').before('<div class="checkbox"><label><input type="checkbox" name="category_new[]" checked value="'+ newCategory +'"/>'+newCategory+'</label></div>');
+            }
+        });
+    });
+});
+</script>
 @stop
